a7d4ac2766aaf5b8483c61424095a6c517d4ce89
sanaeesana15-blip <sanaeesana15@gmail.com>
2025-10-29 20:55:17 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,6 @@
 module.exports = {
-  SESSION_ID: "blinder~5ZBniDrA#Abf_4HAZ-fkFmVb699ffGHwm9LETvaXknX--L9M8wgE",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~9BAAhKaL#UFi6ALbDug2q4sPimAJzxestBrlGUK8gKxI6Bzy80hw
+    ",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
